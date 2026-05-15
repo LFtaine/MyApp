6cf01e0308713b35ccb09e274c522aa0f50c12bc
Louis Fontaine <fontain242@campus.unicaen.fr>
2026-05-15 13:24:45 +0200

    branche main nettoyée

--- tests v1/scripts/bdd.js
+++
@@ -1,9 +0,0 @@
-//modèle: [num,{nom,genre,statut pour moi,auteur,date de début de publication,date de fin de publication,statut,num,statut achat, commentire}]
-// images en jpg
-
-let bdd=[
-    [0,{nom:"One Piece",genre:"Manga",avancee:"fini",auteur:"Eichiro Oda",date_deb:"22/07/1997",date_fin:"--",statut:"En cours",achat:"En cours",commentaire:"Surcoté"}],
-    [1,{nom:"One Piece",genre:"Animé",avancee:"commencé",auteur:"Toei Animation",date_deb:"20/10/1999",date_fin:"--",statut:"En cours",achat:"--",commentaire:""}],
-    [2,{nom:"After God",genre:"Manga",avancee:"fini",auteur:"Sumi Eno",date_deb:"08/2021",date_fin:"--",statut:"En cours",achat:"tout_possede",commentaire:"Incroyable"}],
-    [3,{nom:"Naruto",genre:"Manga",avancee:"fini",auteur:"Masashi Kishimoto",date_deb:"03/10/2002",date_fin:"10/11/2014",statut:"Terminé",achat:"en_cours",commentaire:""}]
-];
